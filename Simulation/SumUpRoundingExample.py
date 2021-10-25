16d4782d0e844a0aadea9a9fa675d55d5fb75a65
K-Enin <kenin@mail.uni-mannheim.de>
2021-10-25 21:54:15 +0200

    Simulation, power grid commit

---
+++ Simulation/SumUpRoundingExample.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 28 16:25:35 2021
+
+@author: katharinaenin
+Example provided in Chapter 3, page 18
+""" 
+import numpy as np
+import matplotlib.pyplot as plt
+
+def standard_rounding(alpha):
+    N, modes = alpha.shape 
+    p = np.zeros((N, modes))
+    for i in range(N):
+        phat = alpha[i,:]
+        j = np.argmax(phat)
+        p[i,j] = 1.
+    return p
+    
+def sum_up_rounding(alpha):
+    'Sum-Up-Rounding with SOS1 constraint on equidistant grid'
+    N, conf = alpha.shape
+    step = 1
+    p = np.zeros((N, conf))
+    array = np.zeros(conf)
+    unique = True
+    for i in range(N):
+        array += step * alpha[i,:]
+        j = np.argmax(array) 
+        if np.sum(array == array[j]) > 1: 
+            unique = False
+        p[i,j] = 1
+        array[j] -= step
+    if not unique:
+        print('\nWarning: Sum-Up Rounding result not unique\n')
+    return p
+
+if __name__ == '__main__':
+    numbers = np.arange(0,8)
+    alpha = np.array([[0, 0.5, 0.25, 0.1,0.9,0.6,0.33,0.8], [1, 0.5, 0.75, 0.9, 0.1, 0.4,0.67,0.2]])
+    alpha = alpha.transpose()
+    alpha1 = standard_rounding(alpha)
+    alpha2 = sum_up_rounding(alpha)
+    
+    plt.figure(1).clear()
+    plt.bar(numbers, alpha[:,0], width = 1, edgecolor='k', color = 'lightcoral', alpha=0.7)
+    plt.yticks(np.arange(0, 1.1, 0.5))
+    plt.xlabel('time i')
+    plt.title('Original values')
+    
+    plt.figure(2).clear()
+    plt.bar(numbers, alpha1[:,0], width = 1, edgecolor='k', color = 'lightcoral', alpha=0.7)
+    plt.yticks(np.arange(0, 1.1, 0.5))
+    plt.xlabel('time i')
+    plt.title('Values calculated by Standard Rounding')
+    
+    plt.figure(3).clear()
+    plt.bar(numbers, alpha2[:,0], width = 1, edgecolor='k', color = 'lightcoral', alpha=0.7)
+    plt.yticks(np.arange(0, 1.1, 0.5))
+    plt.xlabel('time i')
+    plt.title('Values calculated by Sum Up Rounding')
+    
---
+++ powergrid/RunPowerModel.m
@@ -0,0 +1,72 @@
+% Solving Power Model given in PowerModel.m
+global m margin_lower margin_upper P_beg P_end Q_beg Q_end P_at_t Q_at_t;
+m = 300;
+margin_upper = m/2 + 50;
+margin_lower = m/2;
+P_beg = -0.9; 
+P_end = -1.8; 
+Q_beg = -0.3; 
+Q_end = -0.6;
+
+P_at_N5 = generate_P_at_N5(); % takes the values given above
+Q_at_N5 = generate_Q_at_N5();
+
+plot_N5 = true
+
+if plot_N5 == true
+    x_a = 0:1:(m-1);
+    fig1 = figure;
+    plot(x_a,-P_at_N5,'LineWidth',2.0)
+    title('Real Power P at Node N5', 'FontSize',14)
+    xlabel('time steps', 'FontSize',14)
+    ylabel('P (per unit)', 'FontSize',14)
+    axis([0 300 0.7 2])
+    fig2 = figure;
+    plot(x_a,-Q_at_N5,'LineWidth',2.0)
+    title('Reactive Power Q at Node N5', 'FontSize',14)
+    xlabel('time steps', 'FontSize',14)
+    ylabel('Q (per unit)', 'FontSize',14)
+    axis([0 300 0.2 0.8])
+end
+
+solution_matrix = zeros(18,m);
+
+fun = @PowerModel;
+x0 = [0, 0.2, 0.2, 0.2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+
+for t = 1:m
+    P_at_t = P_at_N5(t);
+    Q_at_t = Q_at_N5(t);
+    options = optimoptions('fsolve','Algorithm','levenberg-marquardt')
+    x = fsolve(fun, x0, options)
+    solution_matrix(:,t) = x;
+end
+
+fig3 = figure;
+x_b = 0:1:(m-1);
+plot(x_b, solution_matrix(1,:),'LineWidth',2.0) %non-negative
+title('Results after applying fsolve: Real Power P in Node N1', 'FontSize',14)
+xlabel('time steps', 'FontSize',14)
+ylabel('P (per unit)','FontSize',14)
+axis([0 300 0.6 1.8])
+
+fig4 = figure;
+plot(x_b, solution_matrix(2,:),'LineWidth',2.0)
+title('Results after applying fsolve: Reactive Power Q in Node N1', 'FontSize',14)
+xlabel('time steps', 'FontSize',14)
+ylabel('Q (per unit)', 'FontSize',14)
+
+% Transform P into flow eps
+a0 = 0;
+a1 = -0.5;
+a2 = 10;
+
+eps = a0 + a1*solution_matrix(1,:) + a2*solution_matrix(1,:).*solution_matrix(1,:);
+fig5 = figure;
+plot(x_b, eps,'LineWidth',2.0)
+title('flow $\epsilon$','Interpreter','latex', 'FontSize',15)
+xlabel('time steps', 'FontSize',14)
+ylabel('mass flow ($\frac{kg}{s}$)','Interpreter','latex', 'FontSize',15)
+
+%savdir  = '/Users/katharinaenin/Desktop/Masterarbeit/Code/Optimierungsproblem/eps_files/eps_file3'
+%save(savdir, 'eps')
